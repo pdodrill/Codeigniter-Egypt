30376c139c65148e2cc094c32fbbdae5eb38572e
pdodrill <pdodrill@engineyard.com>
2011-09-12 10:30:27 -0700

    Edited config.php via GitHub

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $config_site_name		= "Site Name";
 
-$db_hostname			= "localhost";
-$db_database			= "egypt";
-$db_username			= "root";
-$db_password			= "1234";
+$db_hostname			= "10.194.111.8";
+$db_database			= "db_452d6210";
+$db_username			= "user_452d6210";
+$db_password			= "u&TN_@Og!$L2JT";
 $db_dbdriver			= "mysql";
